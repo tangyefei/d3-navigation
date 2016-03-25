fe6baea919747c46c8ae423dbe81a88263fd4c20
tangyefei <tyfccsu@gmail.com>
2016-03-25 11:27:17 +0800

    create linkage between different level

--- scripts/runtable.js
+++ scripts/runtable.js
@@ -8,8 +8,24 @@
                 {'id': 'round2', 'class': 'midd-circle', 'radius': 0.5},
                 {'id': 'round3', 'class': 'outer-circle', 'radius': 1.0}
             ]
-        }
+        },
+        'leafConfs': [
+            {'clz': 'dnode', 'radius': 0.5},
+            {'clz': 'tnode',  'radius': 0.8}
+        ],
+        'leafDatas': [],
+        'teamNameOffset': 60
     };
+
+    var tree = d3.layout.tree()
+        .size([360, config.diameter / 2 * config.turntable.rounds[1]['radius'] + 9])
+        .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+    var diagonal = d3.svg.diagonal.radial()
+        .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+    var teamCollection = {}, divisionTeams;
+
 
     /****************************************** TRUN_TABLE ******************************************************/
     function formatGraphUnit(t, x, y, id, attrs) {
@@ -44,38 +60,41 @@
     };
 
     /****************************************** LEAVIES ******************************************************/
-    var tree = d3.layout.tree()
-        .size([360, config.diameter / 2 * config.turntable.rounds[1]['radius'] + 9])
-        .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
-    var diagonal = d3.svg.diagonal.radial()
-        .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+    function processData(data) {
+        if(data && data.children && data.children.length) {
+            for (var i = 0; i < data.children.length; i++) {
+                var division = data.children[i]['name'];
+                var teams = data.children[i]['children'];
+                teamCollection[division] = teams;
+                divisionTeams = divisionTeams || {'name': division, 'children': teams};
+                delete data.children[i]['children'];
+            };
+        }
+    }
 
-    d3.json("data/nba.json", function(error, root) {
-
-      var nodes = tree.nodes(root),
-          links = tree.links(nodes);
-
-      var link = svg.selectAll(".link")
-          .data(links)
-        .enter().append("path")
-          .attr("class", "link")
-          .attr("d", diagonal);
-
-      var node = svg.selectAll(".node")
+    function formatNodesByRound(cont, data) {
+      var nodes = tree.nodes(data);
+      var node = svg.selectAll("." + cont.clz)
           .data(nodes)
         .enter().append("g")
-          .attr("class", "node")
-          .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";})
-
-      node.append("circle")
-          .attr("r", 4.5);
+          .attr("class", cont.clz)
+          .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (cont.radius * config.diameter / 2 - config.teamNameOffset) + ")";})
 
       node.append("text")
           .attr("dy", ".31em")
           .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
           .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
           .text(function(d) { return d.name; });
+      };
+
+
+    d3.json("data/nba.json", function(error, root) {
+        processData(root);
+        config['leafDatas'].push(root);
+        config['leafDatas'].push(divisionTeams);
+        formatNodesByRound(config['leafConfs'][0], config['leafDatas'][0]);
+        formatNodesByRound(config['leafConfs'][1], config['leafDatas'][1]);
     });
 
     formatTurntable();
